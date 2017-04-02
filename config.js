18e87c577c78cb199610652b25e3e5b763731592
Sargun Vohra <sargun.vohra@gmail.com>
2017-04-02 16:52:35 -0400

    Modify for for Purdue and Illinois

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ module.exports = {
   BOARD_URL: 'https://www.reddit.com/api/place/board-bitmap',
   DRAW_URL: 'https://www.reddit.com/api/place/draw.json',
 
-  REMOTE_TARGET_URL: 'https://raw.githubusercontent.com/Zequez/placebot-argentina-target/master/official_target.bmp',
+  REMOTE_TARGET_URL: 'https://raw.githubusercontent.com/sargunster/reddit-placebot/master/target.bmp',
 
   BOARD_H: 1000,
   BOARD_W: 1000,
 
-  autoupdateRemoteTarget: true
+  autoupdateRemoteTarget: false
 }
